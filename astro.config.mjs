6a909fd4a69e19cc273a583a42d96470f1866716
psaul20 <patricksaul20@gmail.com>
2025-10-28 20:32:09 -0500

    updating config for pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/psaul20.io",
+  site: "https://psaul20.github.io",
   base: "/astro-start",
 });
